22895599631b75c28ac4bae4ee339319d12b1602
Brandon Bakr <brandon.bakr@gatech.edu>
2017-10-25 16:07:23 -0400

    Model to be tested implemented

--- nmer.py
+++ nmer.py
@@ -11,6 +11,16 @@ cov_rad = {   'H' : 0.37, 'C' : 0.77, 'O' : 0.73, 'N' : 0.75, 'F' : 0.71,
   'Ni': 0.55, 'Cu': 0.46, 'Zn': 0.60, 'Ga': 1.22, 'Ge': 1.22, 'As': 1.22,
   'Se': 1.17, 'Br': 1.14, 'Kr': 1.03}
 
+def write_geom(name, geom):
+    with open(name, 'w') as fil:
+        lg = len(geom)
+        fil.write(str(lg)+'\n\n')
+        for i in range(lg):
+            atom = geom[i]
+            lin = atom[0]
+            for c in range(1,4): lin+= " "+str(atom[c])
+            fil.write(lin+'\n')
+
 def dist(a,b):
     a_coords = a[1:] 
     b_coords = b[1:]
@@ -51,13 +61,22 @@ def bfs(geom):
     return frags
 
 def frag2str(frag):
-    newfrag = ''
+    #newfrag = ''
+    #frag = list(frag)
+    #for i in frag:
+    #    el = i.split("'")[1]
+    #    coords = i.split(']')[0]
+    #    coords = ''.join(coords.split(',')[1:])
+    #    newfrag += el + coords + '\n'
+    #return newfrag
+    newfrag = []
     frag = list(frag)
     for i in frag:
         el = i.split("'")[1]
         coords = i.split(']')[0]
         coords = ''.join(coords.split(',')[1:])
-        newfrag += el + coords + '\n'
+        coords = [float(x) for x in coords.split()]
+        newfrag.append([el,coords[0],coords[1],coords[2]])
     return newfrag
 
 
@@ -78,44 +97,15 @@ def com(A,els):
     for c in range(3): center[c] = center[c]/totmass
     return center
 
-import numpy as np
-geo_fil = sys.argv[1]
-with open(geo_fil,'r') as fil: 
-    n = int(next(fil))
-    next(fil)
-    geom = []
-    for i in range(n):
-        atom = next(fil).split()
-        for i in range(1,4): atom[i] = float(atom[i])
-        geom.append(atom)
-frags = bfs(geom)
-geoms = [frag2str(i) for i in frags]
-
-for i in range(1):
-    mol0 = sys.argv[1]
-    mol1 = sys.argv[2]
-    sys0 = []
-    sys1 = []
-    with open(mol0,'r') as fil:
-        for lin in fil:
-            if len(lin.split()) != 4: continue
-            sys0.append(lin.split('\n')[0])
-    with open(mol1,'r') as fil:
-        for lin in fil:
-            if len(lin.split()) != 4: continue
-            sys1.append(lin.split('\n')[0])
-# make all dimers and print nre
-#temporarily taking in two xyz's
-#maxn = 5
-#for n in range(2,maxn+1):
+def kabsch(sys0,sys1):
         nsys0 = [] 
         els0 = []
-        for i in sys0: els0.append(i.split()[0].upper())
-        for i in sys0: nsys0.append([float(x) for x in i.split()[1:]])
+        for i in sys0: els0.append(i[0].upper())
+        for i in sys0: nsys0.append([x for x in i[1:]])
         nsys1 = [] 
         els1 = []
-        for i in sys1: els1.append(i.split()[0].upper())
-        for i in sys1: nsys1.append([float(x) for x in i.split()[1:]])
+        for i in sys1: els1.append(i[0].upper())
+        for i in sys1: nsys1.append([x for x in i[1:]])
         #Translate sys1 and sys0 to origin
         center = com(nsys0,els0)
         center1 = com(nsys1,els1)
@@ -125,15 +115,159 @@ for i in range(1):
                 nsys1[i][j] -= center1[j]
         #compute cross-covariance matrix
         H = np.dot(nsys0,np.array(nsys1).T)
+        #H = np.dot(np.array(nsys0).T,np.array(nsys1))
         #SVD to get optimal rotation
         V,s,W = np.linalg.svd(H)
         d = np.linalg.det(np.dot(W,V))
         if d < 0: V[:,-1] *= -1.
         R = np.dot(V,W) 
         #Rotate
+        #nsys1 = np.dot(nsys1,R)
         nsys1 = np.dot(R,nsys1)
         diff = rmsd(nsys0,nsys1)
-        print(diff)
-        for i in range(len(nsys0)): print els0[i],' '.join(map(str,nsys0[i]))
-        for i in range(len(nsys1)): print els0[i],' '.join(map(str,nsys1[i]))
-        
+        return diff
+
+import numpy as np
+
+#for i in range(1):
+#    mol0 = sys.argv[1]
+#    mol1 = sys.argv[2]
+#    sys0 = []
+#    sys1 = []
+#    with open(mol0,'r') as fil:
+#        for lin in fil:
+#            if len(lin.split()) != 4: continue
+#            sys0.append(lin.split('\n')[0])
+#    with open(mol1,'r') as fil:
+#        for lin in fil:
+#            if len(lin.split()) != 4: continue
+#            sys1.append(lin.split('\n')[0])
+## make all dimers and print nre
+##temporarily taking in two xyz's
+##maxn = 5
+##for n in range(2,maxn+1):
+#        #for i in range(len(nsys0)): print els0[i],' '.join(map(str,nsys0[i]))
+#        #for i in range(len(nsys1)): print els0[i],' '.join(map(str,nsys1[i]))
+#    rms = kabsch(sys0,sys1)       
+#    print(rms)
+def distance(atom1, atom2):
+    x = atom1[1]-atom2[1]
+    y = atom1[2]-atom2[2]
+    z = atom1[3]-atom2[3]
+    d = (x*x + y*y + z*z)**0.5
+    return d
+
+temp_symbol = ["X", "H", "HE", "LI", "BE", "B", "C", "N", "O", "F", "NE", "NA", "MG",
+"AL", "SI", "P", "S", "CL", "AR", "K", "CA", "SC", "TI", "V", "CR", "MN", "FE", "CO",
+"NI", "CU", "ZN", "GA", "GE", "AS", "SE", "BR", "KR", "RB", "SR", "Y", "ZR", "NB",
+"MO", "TC", "RU", "RH", "PD", "AG", "CD", "IN", "SN", "SB", "TE", "I", "XE", "CS",
+"BA", "LA", "CE", "PR", "ND", "PM", "SM", "EU", "GD", "TB", "DY", "HO", "ER", "TM",
+"YB", "LU", "HF", "TA", "W", "RE", "OS", "IR", "PT", "AU", "HG", "TL", "PB", "BI",
+"PO", "AT", "RN", "FR", "RA", "AC", "TH", "PA", "U", "NP", "PU", "AM", "CM", "BK",
+"CF", "ES", "FM", "MD", "NO", "LR", "RF", "DB", "SG", "BH", "HS", "MT", "DS", "RG",
+"UUB", "UUT", "UUQ", "UUP", "UUH", "UUS", "UUO"]
+
+z_num = {}
+for i,el in enumerate(temp_symbol): z_num[el] = i
+
+def nuclear_repulsion_energy(mol):
+    e = 0
+    for a1 in range(len(mol)):
+        for a2 in range(a1):
+            z1 = z_num[mol[a1][0]]
+            z2 = z_num[mol[a2][0]]
+            dist = distance(mol[a1],mol[a2])
+            e += z1 * z2 / dist
+    return e 
+
+def choose(n,k):
+    topprod = 1
+    for i in range(n-k+1,n+1): topprod *= i
+    bottomprod = 1
+    for i in range(1,k+1): bottomprod *= i
+    return topprod // bottomprod
+
+def get_nmers(geo_fil_name, N):
+    """ Returns a dictionary of lists that contain Molecule objects for
+        all unique n-mers from n=1-N. Key values correspond to the value of 
+        n.
+    >>> H2O_20.get_nmers(1)
+    {1: [<qcdb.molecule.Molecule object at 0x7f3a66929f10>, <qcdb.molecule.Molecule object at 0x7f3a66929ed0>]}
+    """
+    #Read Geometry
+    with open(geo_fil_name,'r') as fil: 
+        n = int(next(fil))
+        next(fil)
+        geom = []
+        for i in range(n):
+            atom = next(fil).split()
+            for i in range(1,4): atom[i] = float(atom[i])
+            geom.append(atom)
+    #Generate Monomers
+    frags = bfs(geom)
+    geoms = [frag2str(i) for i in frags]
+
+    from itertools import combinations
+    
+    
+    nfrags = len(geoms)
+    if N > nfrags: 
+        raise Exception("BFS only found %d fragments. Cannot find %d-mer with %d fragments." % (nfrags,N,nfrags)) 
+
+    inds = [x for x in range(nfrags)]
+    
+    #dictionary with lists for each class of n-mer
+    unique = {}
+    for i in range(N): unique[i+1] = []
+
+    #for each type of n-mer
+    for i in range(N):
+        #add each unique n-mer
+        nres = {}
+        for combo in combinations(inds,i+1):
+            mol = []
+            for nm in list(combo): mol += geoms[nm]
+            nre = nuclear_repulsion_energy(mol)
+            #tweak to whatever tightness we'd likee
+            nre = round(nre,2)                    
+            try: nres[nre].append(mol)
+            except: nres[nre] = [mol]
+        for nre in nres.keys():
+            lnre = len(nres[nre])
+            #last element not checked for duplication 
+            unique[i+1].append(nres[nre][-1])
+            #nothing to compare too if only one 
+            if lnre == 1: continue
+            for m1 in range(lnre-1):
+                duplicate = False
+                for m2 in range(m1+1,lnre):
+                    mol1 = nres[nre][m1] 
+                    mol2 = nres[nre][m2]
+                    rmsd = kabsch(mol1,mol2) 
+                    #make keyword for this option
+                    if rmsd < 0.01: 
+                        duplicate = True
+                        break
+                if not duplicate: unique[i+1].append(mol1)
+    return unique, len(geom) 
+
+unis, tot_mons = get_nmers("sulfanilamide.xyz", 3) 
+
+uk = sorted(unis.keys())
+
+
+import os
+try: os.mkdir("Unique_Nmers")
+except: pass
+
+print("Total number of monomers: %d" % (tot_mons))
+
+os.chdir("Unique_Nmers")
+for k in uk: 
+    print("Total number of unique %d-mers out of a possible %10d: %5d" % (k,choose(tot_mons,k),len(unis[k])))
+    try: os.mkdir(str(k))
+    except: pass
+    os.chdir(str(k))
+    for num, geom in enumerate(unis[k]): write_geom(str(num+1)+'.xyz', geom)
+    os.chdir("..")
+
